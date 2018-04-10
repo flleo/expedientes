eb35772c482bb18d471fbbf12e6e60d73db7c3cd
fede <fedelleos@yahoo.es>
2018-04-10 12:47:08 +0100

    combo paises

--- js/app.js
+++ js/app.js
@@ -1,158 +1,225 @@
 var app = new Vue({
 
-	el: "#root",
-	data: {
-		showingNuevoExpediente:false,
-		showingEditarExpediente: false,
-		showingDeleteExpediente: false,
-		errorMessage : "",
-		successMessage : "",
-		users: [],
-		newUser: {email: "", mobile: ""},
-		expedientes: [],
-		newExpediente: {idUrgente: 0, idTipoExpediente: 0, fecha: "", numero: "", idTitular:0, idDireccion:0, idProyectista:0,idCalificacion:0, idIAE:0, descripcion: ""},
-		clickedUser: {},
-		clickedExpediente: {},
+    el: "#root",
+    data: {
+        showingRegistrarse: false,
+        showingLogin: false,
+        showingNuevoExpediente: false,
+        showingEditarExpediente: false,
+        showingDeleteExpediente: false,
+        errorMessage: "",
+        successMessage: "",
+        usuario: "",
+       /* users: [],
+        newUser: {email: "", mobile: ""},*/
+        expedientes: [],
+        paises: [],
+        provincias: [],
+        newExpediente: {idUrgente: 0, idTipoExpediente: 0, fecha: "", numero: "", idTitular: 0, idDireccion: 0, idProyectista: 0, idCalificacion: 0, idIAE: 0, descripcion: ""},
+        login : {email: "", contraseña:""},
+        pais : {id:0},
+        //clickedUser: {},
+        clickedExpediente: {},
+        clickedRegistrarse: {},
 
-	},
-	mounted: function () {
-		console.log("Hi KK");
-		this.getAllExpedientes();
-	},
-	methods: {
-	getAllExpedientes: function(){
-		axios.get("http://localhost/expedientes/php/api.php?action=read")
-		.then(function(response){
-			console.log(response);
-			if (response.data.error) {
-				app.errorMessage = response.data.message;
-			}else{
-				app.expedientes = response.data.expedientes;
-			}
-		});
-	},
-	creaExpediente:function(){
+    },
+    mounted: function () {
+        console.log("Expedientes");
+        this.getAllExpedientes();
+        this.getAllPaises();
+        
+        
+    },
+    methods: {
+        //Registrarse//////////
+        registrarse: function () {
+            var formData = app.toFormData(app.clickedRegistrarse);
+            axios.post("http://localhost/expedientes/php/api.php?action=registrarse", formData)
+                    .then(function (response) {
+                        console.log(response);
+                        app.clickedRegistrarse = {};
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                        } else {
+                            app.successMessage = response.data.message;
+                            app.getAllExpedientes();
+                        }
+                    });
+        },
+        getAllPaises: function () {
+            axios.get("http://localhost/expedientes/php/api.php?action=mostrarPaises")
+                    .then(function (response) {
+                        console.log(response);
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                        } else {
+                            app.paises = response.data.paises;
+                        }
+                    });
+        },
+        getProvincias: function () {
+            var formData = app.toFormData(app.clickedRegistrarse);
+            axios.get("http://localhost/expedientes/php/api.php?action=mostrarProvincias")
+                    .then(function (response) {
+                        console.log(response);
+                        app.clickedRegistrarse = {}
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                        } else {
+                            app.provincias = response.data.provincias;
+                        }
+                    });
+        },
+        //Login///////////////
+        comprobarLogin: function () {
+            var formData = app.toFormData(app.login);
+            axios.post("http://localhost/expedientes/php/api.php?action=comprobarLogin", formData)
+                    .then(function (response) {
+                        console.log(response);
+                        app.login = {email:"",contraseña:""};
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                            
+                        } else {
+                            app.successMessage = response.data.message;
+                            
+                        }
+                    });
+        },
+        //Expedientes///////////////
+        getAllExpedientes: function () {
+            axios.get("http://localhost/expedientes/php/api.php?action=mostrarExpedientes")
+                    .then(function (response) {
+                        console.log(response);
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                        } else {
+                            app.expedientes = response.data.expedientes;
+                        }
+                    });
+        },
+        creaExpediente: function () {
 
-		var formData = app.toFormData(app.newExpediente);
-		axios.post("http://localhost/expedientes/php/api.php?action=creaExpediente", formData)
-			.then(function(response){
-				console.log(response);
-				app.newExpediente = {idUrgente: 0, idTipoExpediente: 0, fecha: "", numero: "", idTitular:0, idDireccion:0, idProyectista:0,idCalificacion:0, idIAE:0, descripcion: ""};
-				if (response.data.error) {
-					app.errorMessage = response.data.message;
+            var formData = app.toFormData(app.newExpediente);
+            axios.post("http://localhost/expedientes/php/api.php?action=creaExpediente", formData)
+                    .then(function (response) {
+                        console.log(response);
+                        app.newExpediente = {idUrgente: 0, idTipoExpediente: 0, fecha: "", numero: "", idTitular: 0, idDireccion: 0, idProyectista: 0, idCalificacion: 0, idIAE: 0, descripcion: ""};
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
 
-				}else{
-					app.successMessage = response.data.message;
-					app.getAllExpedientes();
-				}
-			});
-		},
-		updateExpediente:function(){
+                        } else {
+                            app.successMessage = response.data.message;
+                            app.getAllExpedientes();
+                        }
+                    });
+        },
+        updateExpediente: function () {
 
-		var formData = app.toFormData(app.clickedExpediente);
-		axios.post("http://localhost/expedientes/php/api.php?action=updateExpediente", formData)
-			.then(function(response){
-				console.log(response);
-				app.clickedExpediente = {};
-				if (response.data.error) {
-					app.errorMessage = response.data.message;
-				}else{
-					app.successMessage = response.data.message;
-					app.getAllExpedientes();
-				}
-			});
-		},
-		selectExpediente(expediente){
-			app.clickedExpediente = expediente;
-		},
-		deleteExpediente:function(){
+            var formData = app.toFormData(app.clickedExpediente);
+            axios.post("http://localhost/expedientes/php/api.php?action=updateExpediente", formData)
+                    .then(function (response) {
+                        console.log(response);
+                        app.clickedExpediente = {};
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                        } else {
+                            app.successMessage = response.data.message;
+                            app.getAllExpedientes();
+                        }
+                    });
+        },
+        selectExpediente(expediente) {
+            app.clickedExpediente = expediente;
+        },
+        deleteExpediente: function () {
 
-		var formData = app.toFormData(app.clickedExpediente);
-		axios.post("http://localhost/expedientes/php/api.php?action=deleteExpediente", formData)
-			.then(function(response){
-				console.log(response);
-				app.clickedExpediente = {};
-				if (response.data.error) {
-					app.errorMessage = response.data.message;
-				}else{
-					app.successMessage = response.data.message;
-					app.getAllExpedientes();
-				}
-			});
-		}
+            var formData = app.toFormData(app.clickedExpediente);
+            axios.post("http://localhost/expedientes/php/api.php?action=deleteExpediente", formData)
+                    .then(function (response) {
+                        console.log(response);
+                        app.clickedExpediente = {};
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                        } else {
+                            app.successMessage = response.data.message;
+                            app.getAllExpedientes();
+                        }
+                    });
+        }
 
-		,
-		getAllUsers: function(){
-			axios.get("http://localhost/vueCRUD/api.php?action=read")
-			.then(function(response){
-				console.log(response);
-				if (response.data.error) {
-					app.errorMessage = response.data.message;
-				}else{
-					app.users = response.data.users;
-				}
-			});
-		},
-		saveUser:function(){
+        ,
+        getAllUsers: function () {
+            axios.get("http://localhost/vueCRUD/api.php?action=read")
+                    .then(function (response) {
+                        console.log(response);
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                        } else {
+                            app.users = response.data.users;
+                        }
+                    });
+        },
+        saveUser: function () {
 
-			var formData = app.toFormData(app.newUser);
-			axios.post("http://localhost/vueCRUD/api.php?action=create", formData)
-				.then(function(response){
-					console.log(response);
-					app.newUser = {username: "", email: "", mobile: ""};
-					if (response.data.error) {
-						app.errorMessage = response.data.message;
-					}else{
-						app.successMessage = response.data.message;
-						app.getAllUsers();
-					}
-				});
-			},
-			updateUser:function(){
+            var formData = app.toFormData(app.newUser);
+            axios.post("http://localhost/vueCRUD/api.php?action=create", formData)
+                    .then(function (response) {
+                        console.log(response);
+                        app.newUser = {username: "", email: "", mobile: ""};
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                        } else {
+                            app.successMessage = response.data.message;
+                            app.getAllUsers();
+                        }
+                    });
+        },
+        updateUser: function () {
 
-			var formData = app.toFormData(app.clickedUser);
-			axios.post("http://localhost/vueCRUD/api.php?action=update", formData)
-				.then(function(response){
-					console.log(response);
-					app.clickedUser = {};
-					if (response.data.error) {
-						app.errorMessage = response.data.message;
-					}else{
-						app.successMessage = response.data.message;
-						app.getAllUsers();
-					}
-				});
-			},
-			deleteUser:function(){
+            var formData = app.toFormData(app.clickedUser);
+            axios.post("http://localhost/vueCRUD/api.php?action=update", formData)
+                    .then(function (response) {
+                        console.log(response);
+                        app.clickedUser = {};
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                        } else {
+                            app.successMessage = response.data.message;
+                            app.getAllUsers();
+                        }
+                    });
+        },
+        deleteUser: function () {
 
-			var formData = app.toFormData(app.clickedUser);
-			axios.post("http://localhost/vueCRUD/api.php?action=delete", formData)
-				.then(function(response){
-					console.log(response);
-					app.clickedUser = {};
-					if (response.data.error) {
-						app.errorMessage = response.data.message;
-					}else{
-						app.successMessage = response.data.message;
-						app.getAllUsers();
-					}
-				});
-			},
-			selectUser(user){
-				app.clickedUser = user;
-			},
+            var formData = app.toFormData(app.clickedUser);
+            axios.post("http://localhost/vueCRUD/api.php?action=delete", formData)
+                    .then(function (response) {
+                        console.log(response);
+                        app.clickedUser = {};
+                        if (response.data.error) {
+                            app.errorMessage = response.data.message;
+                        } else {
+                            app.successMessage = response.data.message;
+                            app.getAllUsers();
+                        }
+                    });
+        },
+        selectUser(user) {
+            app.clickedUser = user;
+        },
 
-			toFormData: function(obj){
-				var form_data = new FormData();
-			      for ( var key in obj ) {
-			          form_data.append(key, obj[key]);
-			      }
-			      return form_data;
-			},
-			clearMessage: function(){
-				app.errorMessage = "";
-				app.successMessage = "";
-			},
+        toFormData: function (obj) {
+            var form_data = new FormData();
+            for (var key in obj) {
+                form_data.append(key, obj[key]);
+            }
+            return form_data;
+        },
+        clearMessage: function () {
+            app.errorMessage = "";
+            app.successMessage = "";
+        },
 
-	}
+    }
 });
--- php/api.php
+++ php/api.php
@@ -1,90 +1,149 @@
 <?php
 
 $conn = new mysqli("localhost", "root", "", "bdexpedientes");
-if ($conn->connect_error) {
-	die("Database connection established Failed..");
-}
+
+if ($conn->connect_error)
+    die("Database connection established Failed..");
 $res = array('error' => false);
 
+if (isset($_GET['action']))    $action = $_GET['action'];
+
+//Registrarse//////////////////////////////////////////////
+if ($action == 'registrarse') {
+   /* $id = $_POST['id'];
+    $idUrgente = $_POST['idUrgente'];
+    $idTipoExpediente = $_POST['idTipoExpediente'];
+    $fecha = $_POST['fecha'];
+    $numero = $_POST['numero'];
+    $idTitular = $_POST['idTitular'];
+    $idDireccion = $_POST['idDireccion'];
+    $idProyectista = $_POST['idProyectista'];
+    $idCalificacion = $_POST['idCalificacion'];
+    $idIAE = $_POST['idIAE'];
+    $idEstado = $_POST['idEstado'];
+    $descripcion = $_POST['descripcion'];
 
 
-$action = 'read';
+    $result = $conn->query("UPDATE `expediente` SET `idUrgente`=$idUrgente,`idTipoExpediente`=$idTipoExpediente,`fecha`='$fecha',`numero`='$numero',`idTitular`=$idTitular,`idDireccion`=$idDireccion,`idProyectista`=$idProyectista,`idCalificacion`=$idCalificacion,`idIAE`=$idIAE,`idEstado`=$idEstado,`descripcion`='$descripcion' WHERE `id`=$id");
+    if ($result) {
+        $res['message'] = "Expediente Actualizado correctamente";
+    } else {
+        $res['error'] = true;
+        $res['message'] = "La actualización del expediente ha fallado";
+    }
+    * 
+    */
+}
+if ($action == 'mostrarPaises') {
+    $result = $conn->query("SELECT * FROM `pais`");
+    $paises = array();
 
-if (isset($_GET['action'])) {
-	$action = $_GET['action'];
+    while ($row = $result->fetch_assoc()) {
+        array_push($paises, $row);
+    }
+    $res['paises'] = $paises;
+}
+if ($action == 'mostrarProvincias') {
+    $id=$_POST['id'];
+    $result = $conn->query("SELECT * FROM `provincia` WHERE idPais = $id");
+    $provincias = array();
+
+    while ($row = $result->fetch_assoc()) {
+        array_push($provincias, $row);
+    }
+    $res['provincias'] = $provincias;
 }
 
-if ($action == 'read') {
-	$result = $conn->query("SELECT * FROM `expediente`");
-	$expedientes = array();
+//Login/////////////////////////////////////////////////////
+if ($action == 'comprobarLogin') {
+    $email = $_POST['email'];
+    $contraseña = $_POST['contraseña'];
 
-	while ($row = $result->fetch_assoc()){
-		array_push($expedientes, $row);
-	}
-	$res['expedientes'] = $expedientes;
+    $result = $conn->query("SELECT * FROM `usuario` WHERE email like '$email' AND contraseña like '$contraseña'");
+
+    $res['usuario'] = $result->fetch_assoc();
+    if ($res['usuario']!=null) {
+        $res['message'] = "Usuario logueado con éxito";
+    } else {
+        $res['error'] = true;
+        $res['message'] = "Login fallido";
+    }
+}
+
+
+
+//Expedientes//////////////////////////////////////////////////
+if ($action == 'mostrarExpedientes') {
+    $result = $conn->query("SELECT * FROM `expediente`");
+    $expedientes = array();
+
+    while ($row = $result->fetch_assoc()) {
+        array_push($expedientes, $row);
+    }
+    $res['expedientes'] = $expedientes;
 }
 
 if ($action == 'creaExpediente') {
 
-	$idUrgente = $_POST['idUrgente'];
-	$idTipoExpediente = $_POST['idTipoExpediente'];
-	$fecha = $_POST['fecha'];
-	$idTitular = $_POST['idTitular'];
-	$idDireccion = $_POST['idDireccion'];
-	$idProyectista = $_POST['idProyectista'];
-	$idCalificacion = $_POST['idCalificacion'];
-	$idIAE = $_POST['idIAE'];
-	$idEstado = $_POST['idEstado'];
-	$descripcion = $_POST['descripcion'];
+    $idUrgente = $_POST['idUrgente'];
+    $idTipoExpediente = $_POST['idTipoExpediente'];
+    $fecha = $_POST['fecha'];
+    $idTitular = $_POST['idTitular'];
+    $idDireccion = $_POST['idDireccion'];
+    $idProyectista = $_POST['idProyectista'];
+    $idCalificacion = $_POST['idCalificacion'];
+    $idIAE = $_POST['idIAE'];
+    $idEstado = $_POST['idEstado'];
+    $descripcion = $_POST['descripcion'];
 
 
-	$result = $conn->query("INSERT INTO `expediente` (`id`, `idUrgente`, `idTipoExpediente`, `fecha`, `numero`, `idTitular`, `idDireccion`, `idProyectista`, `idCalificacion`, `idIAE`, `idEstado`, `descripcion`)
+    $result = $conn->query("INSERT INTO `expediente` (`id`, `idUrgente`, `idTipoExpediente`, `fecha`, `numero`, `idTitular`, `idDireccion`, `idProyectista`, `idCalificacion`, `idIAE`, `idEstado`, `descripcion`)
 													VALUES (null,$idUrgente, $idTipoExpediente, '$fecha', null, $idTitular,$idDireccion, $idProyectista,$idCalificacion,$idIAE,$idEstado,'$descripcion')");
-	if ($result) {
-		$res['message'] = "Expediente añadido con éxito";
-	} else{
-		$res['error'] = true;
-		$res['message'] = "La inserción ha fallado";
-	}
+    if ($result) {
+        $res['message'] = "Expediente añadido con éxito";
+    } else {
+        $res['error'] = true;
+        $res['message'] = "La inserción ha fallado";
+    }
 }
 
 
 if ($action == 'updateExpediente') {
-	$id = $_POST['id'];
-	$idUrgente = $_POST['idUrgente'];
-	$idTipoExpediente = $_POST['idTipoExpediente'];
-	$fecha = $_POST['fecha'];
-	$numero = $_POST['numero'];
-	$idTitular = $_POST['idTitular'];
-	$idDireccion = $_POST['idDireccion'];
-	$idProyectista = $_POST['idProyectista'];
-	$idCalificacion = $_POST['idCalificacion'];
-	$idIAE = $_POST['idIAE'];
-	$idEstado = $_POST['idEstado'];
-	$descripcion = $_POST['descripcion'];
+    $id = $_POST['id'];
+    $idUrgente = $_POST['idUrgente'];
+    $idTipoExpediente = $_POST['idTipoExpediente'];
+    $fecha = $_POST['fecha'];
+    $numero = $_POST['numero'];
+    $idTitular = $_POST['idTitular'];
+    $idDireccion = $_POST['idDireccion'];
+    $idProyectista = $_POST['idProyectista'];
+    $idCalificacion = $_POST['idCalificacion'];
+    $idIAE = $_POST['idIAE'];
+    $idEstado = $_POST['idEstado'];
+    $descripcion = $_POST['descripcion'];
 
 
-	$result = $conn->query("UPDATE `expediente` SET `idUrgente`=$idUrgente,`idTipoExpediente`=$idTipoExpediente,`fecha`='$fecha',`numero`='$numero',`idTitular`=$idTitular,`idDireccion`=$idDireccion,`idProyectista`=$idProyectista,`idCalificacion`=$idCalificacion,`idIAE`=$idIAE,`idEstado`=$idEstado,`descripcion`='$descripcion' WHERE `id`=$id");
-	if ($result) {
-		$res['message'] = "Expediente Actualizado correctamente";
-	} else{
-		$res['error'] = true;
-		$res['message'] = "La actualización del expediente ha fallado";
-	}
+    $result = $conn->query("UPDATE `expediente` SET `idUrgente`=$idUrgente,`idTipoExpediente`=$idTipoExpediente,`fecha`='$fecha',`numero`='$numero',`idTitular`=$idTitular,`idDireccion`=$idDireccion,`idProyectista`=$idProyectista,`idCalificacion`=$idCalificacion,`idIAE`=$idIAE,`idEstado`=$idEstado,`descripcion`='$descripcion' WHERE `id`=$id");
+    if ($result) {
+        $res['message'] = "Expediente Actualizado correctamente";
+    } else {
+        $res['error'] = true;
+        $res['message'] = "La actualización del expediente ha fallado";
+    }
 }
 
 
 if ($action == 'deleteExpediente') {
-	$id = $_POST['id'];
+    $id = $_POST['id'];
 
 
-	$result = $conn->query("DELETE FROM `expediente` WHERE `id` = $id");
-	if ($result) {
-		$res['message'] = "Expediente eliminado";
-	} else{
-		$res['error'] = true;
-		$res['message'] = "La eliminación del expediente ha fallado";
-	}
+    $result = $conn->query("DELETE FROM `expediente` WHERE `id` = $id");
+    if ($result) {
+        $res['message'] = "Expediente eliminado";
+    } else {
+        $res['error'] = true;
+        $res['message'] = "La eliminación del expediente ha fallado";
+    }
 }
 
 
@@ -95,9 +154,8 @@ if ($action == 'deleteExpediente') {
 
 
 
-$conn -> close();
+$conn->close();
 header("Content-type: application/json");
 echo json_encode($res);
 die();
-
- ?>
+?>
